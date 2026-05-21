42676733631d86cb0004c13667169aeb30b9cba0
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2026-05-22 01:11:51 +0600

    bug fix

--- app/redux/features/auth/authApi.ts
+++ app/redux/features/auth/authApi.ts
@@ -119,5 +119,5 @@ export const {
   useVerifyOtpMutation,
   useRefreshTokenMutation,
   useLogOutMutation,
-  useToggleTwoFactorMutation, // ✅
+  useToggleTwoFactorMutation,
 } = authApi;
--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,20 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./features/auth/authApi";
 import { jobApi } from "../(dashboard)/hr-dashboard/create-job/api/create-job-api";
+import authReducer from "./features/auth/authSlice"; // ✅ যোগ করো
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
-    [jobApi.reducerPath]: jobApi.reducer, //
+    [jobApi.reducerPath]: jobApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      authApi.middleware,
-      jobApi.middleware, //
-    ),
+    getDefaultMiddleware().concat(authApi.middleware, jobApi.middleware),
 });
 
-// TYPES
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
